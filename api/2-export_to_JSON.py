9cea343fc57e1a6054253dbd321b657cee3b0977
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-25 05:05:00 -0800

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,57 +1,35 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0,
-extend your Python script to export
-data in the JSON format.
-
-Requirements:
-
-~ Records all tasks that are owned by
-this employee
-~ Format must be: { "USER_ID":
-[{"task": "TASK_TITLE", "completed":
-~ TASK_COMPLETED_STATUS, "username": "USERNAME"},
-{"task": "TASK_TITLE", "completed":
-TASK_COMPLETED_STATUS, "username":
-"USERNAME"}, ... ]} File name must be:
-USER_ID.json
+Using a REST API and an EMP_ID, save info about their TODO list in a json file
 """
+import json
+import requests
+import sys
+
 if __name__ == "__main__":
-    import csv
-    import json
-    import requests
-    import sys
-    # using this url https://jsonplaceholder.typicode.com/todos/
-    # add a query string of userId = 2 using the requests module
-    url1 = "https://jsonplaceholder.typicode.com/todos"
-    url2 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    payload = {"userId": sys.argv[1]}
-    # a sinle variable used to accept the response
-    # after request is made using the module
-    req_rep1 = requests.get(url1, params=payload)
-    req_rep2 = requests.get(url2)
-    req_rep1 = req_rep1.json()
-    req_rep2 = req_rep2.json()
-    # file name depends on id
-    filename2 = f"{sys.argv[1]}.json"
-    # create a dictionary having the key "userid" and
-    # the value should be an  empty list
-    user_data = {f'{req_rep2["id"]}': []}
-    # now get the keys of the user
-    user_keys = list(req_rep1[0].keys())
-    # now iterate over req_rep1 and req_rep2 to get
-    # to get a dictionary of task/title, completeted, username
-    # key value pairs
-    for data in req_rep1:
-        new_dict = {}
-        for key in user_keys:
-            if key != "title" and key != "completed":
-                continue
-            if key == "title":
-                new_dict["task"] = data[key]
-            else:
-                new_dict[key] = data[key]
-        new_dict["username"] = req_rep2["username"]
-        user_data.get(f"{sys.argv[1]}").append(new_dict)
-    with open(filename2, 'w', encoding='utf-8') as jsonfile:
-        json.dump(user_data, jsonfile)
+    """ Main section """
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if not employee_id:
+        print("Please provide an employee ID as an argument.")
+        sys.exit(1)
+
+    employee = requests.get(f"{BASE_URL}/users/{employee_id}/").json()
+    employee_name = employee.get("username")
+    emp_todos = requests.get(f"{BASE_URL}/users/{employee_id}/todos").json()
+    serialized_todos = []
+
+    for todo in emp_todos:
+        serialized_todos.append({
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": employee_name
+        })
+
+    output_data = {employee_id: serialized_todos}
+
+    with open(f"{employee_id}.json", 'w') as file:
+        json.dump(output_data, file, indent=4)
+
+    print(f"Tasks for employee {employee_id} exported to {file_name}.")
