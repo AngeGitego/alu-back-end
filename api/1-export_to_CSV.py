4a2128b8d83bf547d562a1740f98acc895840e6a
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-25 05:48:23 -0800

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -23,6 +23,7 @@ Second and N next lines display the title of completed tasks:
 TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
 """
 if __name__ == "__main__":
+    import csv
     import json
     import sys
     import urllib.request
